7534c7c9192c00b0899cf4fd1e969e3788ddee03
Katarzyna <kati7@vp.pl>
2019-01-23 19:13:22 +0100

    add basic check

--- src/MagicNumberService.java
+++ src/MagicNumberService.java
@@ -1,6 +1,23 @@
+import java.io.File;
+
 public class MagicNumberService {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("Brak plików.");
+            return;
+        }
+        for (String filePath : args) {
+            // main loop where we are checking each file
+
+            File file = new File(filePath);
+            //sanity checks
+            if (!file.exists() || file.isDirectory()) {
+                System.out.println("File " + filePath
+                        + " is directory or it does not exist.");
+                continue;
+            }
+        }
     }
 }
